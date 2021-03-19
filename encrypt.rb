4466c02f181d73a187a4a9bbc2a3c00d823dc0f8
zvasib <zvasib@ya.ru>
2021-03-19 18:02:10 +0700

    Add sha2

--- encrypt.rb
+++ encrypt.rb
@@ -6,10 +6,12 @@ text_to_encrypt = gets.to_s.chomp
 puts "Каким способом зашифровать:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 print ">"
 variant_encrypt = gets.to_i
 puts "Вот что получилось:"
 case variant_encrypt
 when 1 then puts Digest::MD5.hexdigest text_to_encrypt
 when 2 then puts Digest::SHA1.hexdigest text_to_encrypt
+when 3 then puts Digest::SHA2.hexdigest text_to_encrypt
 end
